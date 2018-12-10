815ce07941ad3b1034c6c0a6c7bfbb6afb10d36d
Inna <inna.vorobiova@siligin.com>
2018-12-10 17:50:41 +0200

    Tower Shield Prefab

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+    [SerializeField] private int health;
+    [SerializeField] private int armor;
+
+
+
+    private void OnMouseEnter()
+    {
+        transform.localScale = new Vector3(120f, 120f, 120f);
+    }
+
+    private void OnMouseExit()
+    {
+        transform.localScale = new Vector3(100f, 100f, 100f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Enemy");
+        }
+    }
+}
